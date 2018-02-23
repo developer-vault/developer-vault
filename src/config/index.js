7aa45d093cf879e79583efa7af83006192830a70
maxence-lefebvre <maxence.lfbvr@gmail.com>
2018-02-23 13:26:22 +0100

    chore(lint): add eslint & stylelint & babel configuration (#8)
    * feat(lint): add eslint & stylelint & babel configuration
    fix lint issues
    
    * chore(babel): use babel@7
    
    * fix(package): remove babel & eslint configs from package.json

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,10 @@
 export default {
-    raven: {
-        dsn: process.env.REACT_APP_SENTRY_DSN,
-        options: {
-            logger: 'developer-vault-client',
-            environment: process.env.REACT_APP_ENV || process.env.NODE_ENV,
-            release: process.env.REACT_APP_VERSION,
-        }
-    }
+  raven: {
+    dsn: process.env.REACT_APP_SENTRY_DSN,
+    options: {
+      logger: 'developer-vault-client',
+      environment: process.env.REACT_APP_ENV || process.env.NODE_ENV,
+      release: process.env.REACT_APP_VERSION,
+    },
+  },
 };
