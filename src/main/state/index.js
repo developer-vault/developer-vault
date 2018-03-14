9c7312a2a2c324d16f2b5b11d3623565d3ae12ff
Gautier Lefebvre <gautier.lfbvr@gmail.com>
2018-03-14 14:36:53 +0100

    feat(store-encryption): create a redux middleware to save the tree on each action

--- src/main/state/index.js
+++ src/main/state/index.js
@@ -1,18 +1,28 @@
-const { SAVE_STATE, LOAD_STATE } = require('common/events');
+const { SET_KEY, SAVE_STATE, LOAD_STATE } = require('common/events');
 
 const { saveFilePath } = require('../config/constants');
 const { on, removeEventListener } = require('../ipc');
 const { encryptToFile, decryptFromFile } = require('./encrypt');
+
+let KEY;
+
+/**
+ * Set the key.
+ *
+ * @param {string} key - The key as an utf8 string.
+ */
+function onSetKey(key) {
+  KEY = key;
+}
 
 /**
  * Handler function for the SAVE_STATE event.
  *
  * @async
  * @param {Object} state - State tree.
- * @param {string} key - Key to encrypt, as an utf8 string.
  */
-function onSaveState(state, key) {
-  return encryptToFile(state, key, saveFilePath);
+function onSaveState(state) {
+  return encryptToFile(state, KEY, saveFilePath);
 }
 
 /**
@@ -28,12 +38,14 @@ function onLoadState(key) {
 
 /** Register the callbacks. */
 function register() {
+  on(SET_KEY, onSetKey);
   on(SAVE_STATE, onSaveState);
   on(LOAD_STATE, onLoadState);
 }
 
 /** Unregister the callbacks. */
 function cleanup() {
+  removeEventListener(SET_KEY, onSetKey);
   removeEventListener(SAVE_STATE, onSaveState);
   removeEventListener(LOAD_STATE, onLoadState);
 }
