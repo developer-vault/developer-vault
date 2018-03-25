060868c55230e9fbd0f040f48a00def6438b0e52
sylvainar <sylvain.ar@hotmail.fr>
2018-03-25 16:00:07 +0200

    feat(nodes): setup algorithm for tree build

--- src/renderer/services/node/index.js
+++ src/renderer/services/node/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/prefer-default-export */
 import uuid from 'uuid/v4';
 
 /**
@@ -12,4 +11,30 @@ export const create = (node) => {
   return {
     [uniqueId]: { ...node, id: uniqueId },
   };
+};
+
+/**
+ * Given the hashmap of nodes, build the tree.
+ *
+ * @param {Object} nodes - Nodes hashmap.
+ * @returns {Array} - Tree.
+ * @see index.spec.js for implementation and data structure.
+ */
+export const buildTree = (nodes) => {
+  /**
+   * Recursive function finding children for each node.
+   *
+   * @param {string | null} nodeId - Id of the node.
+   * @param {Object} nodesHashmap - Hashmap of nodes.
+   * @returns {Object[]} - Array of nodes with child in it.
+   */
+  const fetchChildren = (nodeId, nodesHashmap) => Object.values(nodesHashmap)
+    .filter(node => node.parentId === nodeId)
+    .map(node => ({
+      ...node,
+      children: fetchChildren(node.id, nodesHashmap),
+    }));
+
+  // Init recursion by getting all nodes without parent.
+  return fetchChildren(null, nodes);
 };
--- src/renderer/services/node/index.spec.js
+++ src/renderer/services/node/index.spec.js
@@ -1,6 +1,4 @@
-import { create } from './index';
-
-// jest.mock('uuid/v4');
+import { create, buildTree } from './index';
 
 describe('Node service', () => {
   it('create', () => {
@@ -11,5 +9,77 @@ describe('Node service', () => {
         label: 'node1',
       },
     });
+  });
+
+  it('buildTree', () => {
+    /*
+     * node1
+     * - node11
+     * - node12
+     * -- node121
+     * -- node122
+     * node2
+     */
+    const nodes = {
+      node1: {
+        id: 'node1',
+        parentId: null,
+      },
+      node2: {
+        id: 'node2',
+        parentId: null,
+      },
+      node11: {
+        id: 'node11',
+        parentId: 'node1',
+      },
+      node12: {
+        id: 'node12',
+        parentId: 'node1',
+      },
+      node121: {
+        id: 'node121',
+        parentId: 'node12',
+      },
+      node122: {
+        id: 'node122',
+        parentId: 'node12',
+      },
+    };
+
+    expect(buildTree(nodes)).toMatchObject([
+      {
+        id: 'node1',
+        parentId: null,
+        children: [
+          {
+            id: 'node11',
+            parentId: 'node1',
+            children: [],
+          },
+          {
+            id: 'node12',
+            parentId: 'node1',
+            children: [
+              {
+                id: 'node121',
+                parentId: 'node12',
+                children: [],
+              },
+              {
+                id: 'node122',
+                parentId: 'node12',
+                children: [],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        id: 'node2',
+        parentId: null,
+        children: [],
+      },
+    ]);
   });
 });
