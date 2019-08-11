ecc700cb317da1dcf8fc37d19d8a452e267a3b25
Gautier Lefebvre <gautier.lfbvr@gmail.com>
2019-08-11 16:38:53 +0200

    refactor(ipc): refactor ipc promise wrapper

--- src/renderer/services/ipc/index.js
+++ src/renderer/services/ipc/index.js
@@ -1,44 +1,10 @@
-import { uniqueId } from 'lodash';
 import { ipcRenderer } from 'electron';
 
-export const send = async (channel, ...args) => {
-  const id = uniqueId(`ipc_${channel}_`);
+import { makeIpcPromiseWrapper } from 'shared/services/ipc';
 
-  return new Promise((resolve, reject) => {
-    // request
-    ipcRenderer.send(
-      channel,
-      {
-        id,
-        payload: args,
-      },
-    );
-
-    // wait for resolution or rejection
-    ipcRenderer.once(`${id}/result`, (event, data) => {
-      if (data.type === 'resolve') {
-        resolve(data.payload);
-      } else {
-        reject(data.payload);
-      }
-    });
-  });
-};
-
-export const responseMessageHandler = callback => async (event, { id, payload }) => {
-  try {
-    return event.sender.send(`${id}/result`, {
-      id,
-      type: 'resolve',
-      payload: await callback(...payload),
-    });
-  } catch (err) {
-    return event.sender.send(`${id}/result`, {
-      id,
-      type: 'reject',
-      payload: err,
-    });
-  }
-};
-
-export const on = (channel, callback) => ipcRenderer.on(channel, responseMessageHandler(callback));
+export { responseMessageHandler } from 'shared/services/ipc';
+export const {
+  send,
+  on,
+  removeListener,
+} = makeIpcPromiseWrapper(ipcRenderer);
